7354a8ce74b6eec6c992bc16030487c8820e0d6e
ahmedkrr <ahmadbukr2111@gmail.com>
2023-05-29 13:02:55 +0300

    create Directors Endpoint

--- CMDB-BackEnd/Domain/Data/Directors.cs
+++ CMDB-BackEnd/Domain/Data/Directors.cs
@@ -3,9 +3,17 @@
     public class Directors
     {
         public int Directors_Id { get; private set; }
-        public int Directors_Name { get; private set; }
+        public string Directors_Name { get; private set; }
         public ICollection<Movie_Directors> Movie_Directors { get; private set; }
         public ICollection<Series_Directors> Series_Directors { get; private set; }
         public ICollection<TvShow_Directors> TvShow_Directors { get; private set; }
+
+
+        public Directors(string Directors_Name)
+        {
+            this.Directors_Name = Directors_Name;
+        }
+
+
     }
 }
---
+++ CMDB-BackEnd/Resource/Directors/CreateDirectors.cs
@@ -0,0 +1,43 @@
+﻿using CMDB_BackEnd.Domain;
+using CMDB_BackEnd.Domain.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CMDB_BackEnd.Resource
+{
+    [ApiController]
+    [Route("api/directors")]
+    public class CreateDirectors : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CreateDirectors(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost("CreateDirectors")]
+        public async Task<object> CreateDirectorsReq([FromBody]string directorName)
+        {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(directorName))
+                    return BadRequest("The name should not be null");
+
+                var director = new Directors(directorName);
+
+                _context.Add(director);
+                await _context.SaveChangesAsync();
+
+                return Ok("Director added successfully");
+
+            }
+            catch (Exception ex) {
+
+                return StatusCode(StatusCodes.Status500InternalServerError, "An error occurred while adding the director" + ex) ;
+            }
+
+        }
+
+
+    }
+}
