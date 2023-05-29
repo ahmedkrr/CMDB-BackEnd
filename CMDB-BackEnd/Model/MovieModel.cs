1945dc3c5f05faaddfbbb34cef2d440baa6ed5c5
ahmedkrr <ahmadbukr2111@gmail.com>
2023-05-29 17:42:10 +0300

    Get All Movie Endpoint

---
+++ CMDB-BackEnd/Model/MovieModel.cs
@@ -0,0 +1,47 @@
+﻿namespace CMDB_BackEnd.Model
+{
+    public class MovieModel
+    {
+    }
+    public class CreateMovieRequest
+    {
+        public required string Title { get;  set; }
+        public DateTime Release_Date { get;  set; }
+        public required string OverView { get;  set; }
+        public double Runtime { get; set; }
+        public string? Poster_Url { get;  set; }
+        public double Average_Rating { get; set; }
+        public int MediaTypeId { get; set; }
+        public required List<int> Movie_Actors_Id { get; set; }
+        public required List<int> Movie_Direcotr_Id { get; set; }
+        public required List<int> Movie_Genre_Id { get; set; }
+    }
+
+    public class GetAllMovieRequest
+    {
+        public int Movie_Id { get; set; }
+        public required string Title { get; set; }
+        public required string Release_Date { get; set; }
+        public required string OverView { get; set; }
+        public double Runtime { get; set; }
+        public string? Poster_Url { get; set; }
+        public double Average_Rating { get; set; }
+        public string? MediaTypes { get; set; }
+        public List<GetMovieDirector>? MovieDirectors { get; set; }
+        public List<GetMovieActors>? Movie_Actors { get; set; }
+        public List<GetMovieGenre>? MovieGenres { get; set; }
+    }
+
+    public class GetMovieDirector
+    {
+        public string? Directors_Name { get; set; }
+    }
+    public class GetMovieActors
+    {
+        public string?  Actor_Name { get; set; }
+    }
+    public class GetMovieGenre
+    {
+        public string? Movie_genre { get; set; }
+    }
+}
---
+++ CMDB-BackEnd/Resource/Movie/GetAllMovie.cs
@@ -0,0 +1,69 @@
+﻿using CMDB_BackEnd.Domain;
+using CMDB_BackEnd.Domain.Data;
+using CMDB_BackEnd.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.EntityFrameworkCore;
+
+namespace CMDB_BackEnd.Resource
+{
+    [ApiController]
+    [Route("api/Movie")]
+    public class GetAllMovie : ControllerBase
+    {
+        private readonly ApplicationDbContext _Context;
+        public GetAllMovie(ApplicationDbContext context) 
+        {
+            _Context = context; 
+        }
+
+        [HttpGet("GetAllMovie")]
+        public async Task<object> GetAllMovieReq()
+        {
+            try
+            {
+                var movies = await _Context.Set<Movie>()
+                    .Include(c => c.Movie_Actors)
+                    .Include(c => c.Movie_Directors)
+                    .Include(c => c.Movie_Genres)
+                    .Select(c => new GetAllMovieRequest
+                    {
+                        Movie_Id = c.Movie_Id,
+                        Title = c.Title,
+                        Release_Date = c.Release_Date.Year.ToString(),
+                        OverView = c.OverView,
+                        Runtime = c.Runtime,
+                        Poster_Url = c.Poster_Url,
+                        Average_Rating = c.Average_Rating,
+                        MediaTypes = c.MediaType.Media_Type_Name,
+
+                        MovieDirectors = c.Movie_Directors.Select(c => new GetMovieDirector
+                        {
+                            Directors_Name = c.Directors.Directors_Name
+                        }).ToList(),
+
+                        Movie_Actors = c.Movie_Actors.Select(c => new GetMovieActors
+                        {
+                            Actor_Name = c.Actors.Actors_Name
+                        }).ToList(),
+
+                        MovieGenres = c.Movie_Genres.Select(c => new GetMovieGenre
+                        {
+                            Movie_genre = c.Genres.Genre_Name
+                        }).ToList()
+
+                    })
+                .ToListAsync();
+
+                return movies;
+            }
+            catch 
+            (Exception ex) 
+            { 
+                return BadRequest(ex.Message);
+            }
+          
+        }
+
+    }
+}
