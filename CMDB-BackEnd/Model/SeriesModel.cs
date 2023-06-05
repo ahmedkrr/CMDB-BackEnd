f0f6c146d46d1297c33444abe5b842b7ba96865f
ahmedkrr <ahmadbukr2111@gmail.com>
2023-06-05 13:44:44 +0300

    Get All Series Endpoint

--- CMDB-BackEnd/Model/SeriesModel.cs
+++ CMDB-BackEnd/Model/SeriesModel.cs
@@ -17,6 +17,32 @@
         public required List<int> Series_Directors_Id { get; set; }
         public required List<int> Series_Genres_Id { get; set; }
     }
+    public class GetAllSeriesRequest
+    {
+        public int Series_Id { get; set; }
+        public required string Title { get; set; }
+        public required string Release_Date { get; set; }
+        public required string OverView { get; set; }
+        public string? Poster_Url { get; set; }
+        public double Average_Rating { get; set; }
+        public string? MediaTypes { get; set; }
+        public List<GetSeriesDirector>? SeriesDirectors { get; set; }
+        public List<GetSeriesActors>? Series_Actors { get; set; }
+        public List<GetSeriesGenre>? SeriesGenres { get; set; }
+    }
+
+    public class GetSeriesDirector
+    {
+        public string? Directors_Name { get; set; }
+    }
+    public class GetSeriesActors
+    {
+        public string? Actor_Name { get; set; }
+    }
+    public class GetSeriesGenre
+    {
+        public string? Series_genre { get; set; }
+    }
 
 
 }
---
+++ CMDB-BackEnd/Resource/Series/GetAllSeries.cs
@@ -0,0 +1,65 @@
+﻿using CMDB_BackEnd.Domain;
+using CMDB_BackEnd.Domain.Data;
+using CMDB_BackEnd.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CMDB_BackEnd.Resource
+{
+    [ApiController]
+    [Route("api/series")]
+    public class GetAllSeries : ControllerBase
+    {
+        private readonly ApplicationDbContext _Context;
+        public GetAllSeries(ApplicationDbContext context) 
+        { 
+            _Context = context;
+        }
+        [HttpGet("GetAllSeries")]
+        public async Task<object> GetAllSeriesReq()
+        {
+            try
+            {
+                var series = await _Context.Set<Series>()
+                    .Include(c => c.Series_Actors)
+                    .Include(c => c.Series_Directors)
+                    .Include(c => c.Series_Genres)
+                    .Select(c => new GetAllSeriesRequest
+                    {
+                        Series_Id = c.Series_Id,
+                        Title = c.Title,
+                        Release_Date = c.Release_Date.Year.ToString(),
+                        OverView = c.OverView,
+                        Poster_Url = c.Poster_Url,
+                        Average_Rating = c.Average_Rating,
+                        MediaTypes = c.MediaType.Media_Type_Name,
+
+                        SeriesDirectors = c.Series_Directors.Select(c => new GetSeriesDirector
+                        {
+                            Directors_Name = c.Directors.Directors_Name
+                        }).ToList(),
+
+                        Series_Actors = c.Series_Actors.Select(c => new GetSeriesActors
+                        {
+                            Actor_Name = c.Actors.Actors_Name
+                        }).ToList(),
+
+                        SeriesGenres = c.Series_Genres.Select(c => new GetSeriesGenre
+                        {
+                            Series_genre = c.Genres.Genre_Name
+                        }).ToList()
+
+                    })
+                .ToListAsync();
+
+                return Ok(series);
+            }
+            catch
+            (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+        }
+    }
+}
